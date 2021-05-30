55f196c1a488b758034f278e8d093f3f7828c6a6
Vijay Kumar Y <vijay@Vijays-MacBook-Pro.local>
2021-05-30 21:16:51 +0530

    Filter constant columns

--- FilterMethods.py
+++ FilterMethods.py
@@ -20,11 +20,12 @@ print(df.head())
 print(df.dtypes)
 
 # %% [markdown]
+# ## Constant features
 # ### Split the data in to train, test split. In this housing data, "SalePrice" is the dependent/target feature.
 
 # %%
 from sklearn.model_selection import train_test_split
-X_train, y_train, X_test, y_test = train_test_split(df.drop(['SalePrice'], axis=1), df['SalePrice'], test_size=0.3, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(df.drop(['SalePrice'], axis=1), df['SalePrice'], test_size=0.3, random_state=0)
 
 # %% [markdown]
 # ### Select the numeric columns only
@@ -60,4 +61,27 @@ print('Lenght of X train columns: ', len(X_train.columns))
 print('Lenght of numeric X train columns: ', len(numeric_X_train.columns))
 print('Lenght of constant columns: ', len(constant_columns))
 
+# %%
+'''
+for column in X_train.columns:
+    print(X_train[column].dtype)
+#
+print(X_train['LandContour'].unique())
+print(len(X_train['LandContour'].unique()))
+'''
+constant_categorical_columns = [column for column in X_train.columns
+                                if (X_train[column].dtype == "object" and  len(X_train[column].unique()) == 1)]
+print("Constant categorical columns:")
+print(constant_categorical_columns)
+#
+all_constant_columns = constant_columns + constant_categorical_columns
+print("All Constant columns:")
+print(all_constant_columns)
+
+# %% [markdown]
+# ### Drop all constant columns from X_train and X_text
+
+# %%
+X_train.drop(labels=all_constant_columns, axis=1, inplace=True)
+X_test.drop(labels=all_constant_columns, axis=1, inplace=True)
 # %%
