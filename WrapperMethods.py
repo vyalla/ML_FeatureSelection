c43c63c887497ee8ce424f678a0d34662564dfef
Vijay Kumar Y <vijay@Vijays-MacBook-Pro.local>
2021-06-06 13:33:16 +0530

    Initial WrapperMethods with Forward Feature Selection

---
+++ WrapperMethods.py
@@ -0,0 +1,51 @@
+# %% [markdown]
+# ## Import necessary packages and libraries
+
+# %%
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from mlxtend.feature_selection import SequentialFeatureSelector
+from sklearn.ensemble import RandomForestClassifier
+
+# %%
+# Load the Housing data
+df = pd.read_csv('./data/housing.csv')
+print(df.head())
+
+# %%
+# Split the data into train and test
+X_train, X_test, y_train, y_test = train_test_split(df.drop(labels=['SalePrice'], axis=1), df['SalePrice']
+                                    , random_state=0, test_size=0.3)
+print(X_train.head())
+print(y_train.head())
+
+# %% [markdown]
+# ## Forward Feature Selection
+
+# %%
+sfs = SequentialFeatureSelector(RandomForestClassifier(), k_features=10, forward=True, 
+        floating=False, scoring='accuracy', cv=2)
+# fit the object to the training data
+sfs = sfs.fit(X_train, y_train)
+
+# %%
+# Print the selected features.
+selected_features = X_train.columns[list(sfs.k_feature_idx_)]
+print(selected_features)
+
+# %%
+# Print final prediction score
+print(sfs.k_score_)
+
+# %%
+# Transform the newly selected features
+X_train_sfs = sfs.transform(X_train)
+X_test_sfs = sfs.transform(X_test)
+
+# %%
+print(X_test_sfs)
+print(X_test_sfs[0, :])
+print(X_test_sfs[0])
+
+# %%
